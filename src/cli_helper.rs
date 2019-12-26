09306643335f06f0b6f2cb0f0c89b536f79bf882
PicoJr <PicoJr@notanemail.com>
2019-12-26 19:48:21 +0100

    Update cli version to match crate version

--- src/cli_helper.rs
+++ src/cli_helper.rs
@@ -8,7 +8,7 @@ pub struct ActivityCli {}
 impl ActivityCli {
     pub fn get_app(&self) -> App {
         App::new("RTW")
-            .version("1.0")
+            .version("0.1.1")
             .author("PicoJr")
             .about("rust time tracking CLI")
             .arg(
