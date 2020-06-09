1e7cd34770e0e681c45bb9c11557d77c19c58edf
PicoJr <picojr_dev@gmx.com>
2020-06-09 22:54:09 +0200

    Add release.bash

---
+++ release.bash
@@ -0,0 +1,9 @@
+#!/bin/bash -v
+
+RELEASE_DIR="release$(date +%F-%H-%M-%S)"
+
+rustfmt --check src/**/*.rs &&
+    cargo test &&
+    cargo build --release &&
+    mkdir -p ${RELEASE_DIR} &&
+    cp -r CHANGELOG.md commands.md example/ img/ README.md LICENSE target/release/rtw ${RELEASE_DIR}
