57d4a9cefa16094e2eb3da032442ea969f97dc8d
PicoJr <picojr_dev@gmx.com>
2020-07-19 12:33:34 +0200

    Update release.bash, generate musl binary

--- release.bash
+++ release.bash
@@ -4,6 +4,9 @@ RELEASE_DIR="release$(date +%F-%H-%M-%S)"
 
 rustfmt --check src/**/*.rs &&
     cargo test &&
-    cargo build --release &&
+    docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder cargo build --release &&
     mkdir -p ${RELEASE_DIR} &&
-    cp -r ./CHANGELOG.md ./commands.md ./shell-completion.md example/ img/ ./README.md ./LICENSE target/release/rtw ${RELEASE_DIR}
+    cp -r ./CHANGELOG.md ./commands.md ./shell-completion.md \
+        example/ img/ ./README.md ./LICENSE ${RELEASE_DIR} &&
+    cp target/x86_64-unknown-linux-musl/release/rtw ${RELEASE_DIR}/rtw-x86_64-unknown-linux-musl
+
